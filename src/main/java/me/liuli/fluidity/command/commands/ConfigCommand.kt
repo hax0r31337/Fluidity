f397c906be397c7ae9e5070ea64a79a16385b535
liulihaocai <liulihaocaiqwq@gmail.com>
2022-06-01 18:28:21 +0800

    better config command

--- src/main/java/me/liuli/fluidity/command/commands/ConfigCommand.kt
+++ src/main/java/me/liuli/fluidity/command/commands/ConfigCommand.kt
@@ -4,7 +4,7 @@ import me.liuli.fluidity.Fluidity
 import me.liuli.fluidity.command.Command
 
 class ConfigCommand : Command("config", "Manage configs of the client") {
-    private val rootSyntax = "<load/save/reload>"
+    private val rootSyntax = "<load/save/list/reload>"
 
     override fun exec(args: Array<String>) {
         if (args.isEmpty()) {
@@ -26,12 +26,44 @@ class ConfigCommand : Command("config", "Manage configs of the client") {
                 chat("Config §l${Fluidity.configManager.nowConfig}§r saved")
             }
 
+            "list" -> {
+                chat("Configs:")
+                Fluidity.configManager.configPath.listFiles()
+                    .filter { it.isFile }
+                    .map { f ->
+                        f.name.let { if (it.endsWith(".json")) it.substring(0, it.length - 5) else it }
+                    }
+                    .forEach {
+                        chat("  " + if (it == Fluidity.configManager.nowConfig) "§3§l$it§r" else "§c$it")
+                    }
+            }
+
             "reload" -> {
                 Fluidity.configManager.reload()
                 chat("Config §l${Fluidity.configManager.nowConfig}§r reloaded")
             }
 
             else -> chatSyntax(rootSyntax)
+        }
+    }
+
+    override fun tabComplete(args: Array<String>): List<String> {
+        if (args.isEmpty()) return emptyList()
+
+        return when (args.size) {
+            1 -> listOf("load", "save", "list", "reload").filter { it.startsWith(args[0], true) }
+            2 -> when (args[0].lowercase()) {
+                "load" -> {
+                    (Fluidity.configManager.configPath.listFiles() ?: return emptyList())
+                        .filter { it.isFile }
+                        .map { f ->
+                            f.name.let { if (it.endsWith(".json")) it.substring(0, it.length - 5) else it }
+                        }
+                        .filter { it.startsWith(args[1], true) }
+                }
+                else -> emptyList()
+            }
+            else -> emptyList()
         }
     }
 }
