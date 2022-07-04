6ae68926960c8a9c3bf14b31c6b6c1d08b757da4
liulihaocai <liulihaocaiqwq@gmail.com>
2022-07-04 22:05:06 +0800

    fly noclip

--- src/main/java/me/liuli/fluidity/module/modules/movement/Fly.kt
+++ src/main/java/me/liuli/fluidity/module/modules/movement/Fly.kt
@@ -4,6 +4,7 @@ import me.liuli.fluidity.event.EventMethod
 import me.liuli.fluidity.event.UpdateEvent
 import me.liuli.fluidity.module.Module
 import me.liuli.fluidity.module.ModuleCategory
+import me.liuli.fluidity.module.value.BoolValue
 import me.liuli.fluidity.module.value.FloatValue
 import me.liuli.fluidity.module.value.ListValue
 import me.liuli.fluidity.util.client.displayAlert
@@ -19,9 +20,19 @@ class Fly : Module("Fly", "Make you like a bird", ModuleCategory.MOVEMENT) {
     private val modeValue = ListValue("Mode", arrayOf("Vanilla"), "Vanilla")
     private val hSpeedValue = FloatValue("HorizonSpeed", 1.0f, 0.0f, 5.0f)
     private val vSpeedValue = FloatValue("VerticalSpeed", 0.5f, 0.0f, 5.0f)
+    private val noClipValue = BoolValue("NoClip", false)
+
+    override fun onDisable() {
+        if (noClipValue.get()) {
+            mc.thePlayer.noClip = false
+        }
+    }
 
     @EventMethod
     fun onUpdate(event: UpdateEvent) {
+        if (noClipValue.get()) {
+            mc.thePlayer.noClip = true
+        }
         mc.thePlayer.fallDistance = 0f
         when (modeValue.get()) {
             "Vanilla" -> {
